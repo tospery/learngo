533b52eb2dd0d3f7195e46c50ceb8d727a543bd3
tospery <tospery@gmail.com>
2024-02-29 13:43:01 +0800

    指针

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	name string
+	age  int
+}
+
+func setDefaultIfNeed(p *Person) {
+	if len(p.name) == 0 {
+		p.name = "undefined"
+	}
+}
+
+func main() {
+	/*
+		指针
+		go语言的指针不能直接用于计算，相对于C语言，它是一个阉割版，更多功能专门放在了unsafe包
+	*/
+	p := Person{age: 18}
+	setDefaultIfNeed(&p)
+	fmt.Println(p)
 }
