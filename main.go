4a4c6f0bfc9c933d9abe4888ac6fa38a0f69a551
tospery <tospery@gmail.com>
2024-03-02 11:34:17 +0800

    容器

--- main.go
+++ main.go
@@ -2,23 +2,20 @@ package main
 
 import "fmt"
 
-type Person struct {
-	name string
-	age  int
+type Sayer interface {
+	say()
 }
 
-func setDefaultIfNeed(p *Person) {
-	if len(p.name) == 0 {
-		p.name = "undefined"
-	}
+type dog struct{}
+
+func (d *dog) say() {
+	fmt.Println("汪汪汪")
 }
 
 func main() {
 	/*
-		指针
-		go语言的指针不能直接用于计算，相对于C语言，它是一个阉割版，更多功能专门放在了unsafe包
+		接口（interface）
 	*/
-	p := Person{age: 18}
-	setDefaultIfNeed(&p)
-	fmt.Println(p)
+	var sayer Sayer = &dog{}
+	sayer.say()
 }
