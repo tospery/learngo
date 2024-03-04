3438e69cc6fd3dbecf4186a1c6c1291edfb13a62
tospery <tospery@gmail.com>
2024-03-04 19:42:15 +0800

    并发编程

--- main.go
+++ main.go
@@ -1,21 +1,4 @@
 package main
 
-import "fmt"
-
-type Sayer interface {
-	say()
-}
-
-type dog struct{}
-
-func (d *dog) say() {
-	fmt.Println("汪汪汪")
-}
-
 func main() {
-	/*
-		接口（interface）
-	*/
-	var sayer Sayer = &dog{}
-	sayer.say()
 }
